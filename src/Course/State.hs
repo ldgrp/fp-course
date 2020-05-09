b4069848a11c37c08cc62ecd79a4b19eee3f8245
Leo Orpilla III <leoorpilla3@gmail.com>
2020-05-09 21:40:16 +1000

    solve State.hs

--- src/Course/State.hs
+++ src/Course/State.hs
@@ -38,8 +38,7 @@ exec ::
   State s a
   -> s
   -> s
-exec =
-  error "todo: Course.State#exec"
+exec (State state) = snd . state
 
 -- | Run the `State` seeded with `s` and retrieve the resulting value.
 --
@@ -48,8 +47,7 @@ eval ::
   State s a
   -> s
   -> a
-eval =
-  error "todo: Course.State#eval"
+eval (State state) = fst. state
 
 -- | A `State` where the state also distributes into the produced value.
 --
@@ -57,8 +55,7 @@ eval =
 -- (0,0)
 get ::
   State s s
-get =
-  error "todo: Course.State#get"
+get = State (\s -> (s, s))
 
 -- | A `State` where the resulting state is seeded with the given value.
 --
@@ -67,8 +64,7 @@ get =
 put ::
   s
   -> State s ()
-put =
-  error "todo: Course.State#put"
+put v = State (\_s -> ((), v))
 
 -- | Implement the `Functor` instance for `State s`.
 --
@@ -79,8 +75,11 @@ instance Functor (State s) where
     (a -> b)
     -> State s a
     -> State s b
-  (<$>) =
-    error "todo: Course.State#(<$>)"
+  f <$> (State state) = 
+    State (\s -> 
+      let (a, s') = state s
+       in (f a, s')
+    )
 
 -- | Implement the `Applicative` instance for `State s`.
 --
@@ -96,14 +95,17 @@ instance Applicative (State s) where
   pure ::
     a
     -> State s a
-  pure =
-    error "todo: Course.State pure#instance (State s)"
+  pure a = State (\s -> (a, s))
   (<*>) ::
     State s (a -> b)
     -> State s a
     -> State s b
-  (<*>) =
-    error "todo: Course.State (<*>)#instance (State s)"
+  State f <*> State state =
+    State (\s ->
+      let (f', s') = f s
+          (a', s'') = state s'
+      in (f' a', s'')
+    )
 
 -- | Implement the `Monad` instance for `State s`.
 --
@@ -120,8 +122,11 @@ instance Monad (State s) where
     (a -> State s b)
     -> State s a
     -> State s b
-  (=<<) =
-    error "todo: Course.State (=<<)#instance (State s)"
+  f =<< (State state) =
+    State (\s ->
+      let (a, s') = state s
+       in runState (f a) s'
+    )
 
 -- | Find the first element in a `List` that satisfies a given predicate.
 -- It is possible that no element is found, hence an `Optional` result.
@@ -142,8 +147,12 @@ findM ::
   (a -> f Bool)
   -> List a
   -> f (Optional a)
-findM =
-  error "todo: Course.State#findM"
+findM pred (x:.xs) = do
+  b <- pred x
+  if b
+  then pure (Full x)
+  else findM pred xs
+findM _pred Nil = pure Empty
 
 -- | Find the first element in a `List` that repeats.
 -- It is possible that no element repeats, hence an `Optional` result.
@@ -156,8 +165,10 @@ firstRepeat ::
   Ord a =>
   List a
   -> Optional a
-firstRepeat =
-  error "todo: Course.State#firstRepeat"
+firstRepeat list = 
+  let t = trav (\x -> State (\s -> (bool Nil (x:.Nil) (S.member x s), S.insert x s))) list
+  in 
+    find (const True) (flatten (eval t S.empty))
 
 -- | Remove all duplicate elements in a `List`.
 -- /Tip:/ Use `filtering` and `State` with a @Data.Set#Set@.
@@ -169,8 +180,18 @@ distinct ::
   Ord a =>
   List a
   -> List a
-distinct =
-  error "todo: Course.State#distinct"
+distinct list = 
+  let t = trav (\x -> State (\s -> (bool (x:.Nil) Nil (S.member x s), S.insert x s))) list
+  in 
+    flatten (eval t S.empty)
+
+-- traverse
+trav :: Monad k => (a -> k b) -> List a -> k (List b)
+trav _f Nil = pure Nil
+trav f (x:.xs) = do
+  x' <- f x
+  xs' <- trav f xs
+  pure (x':.xs')
 
 -- | A happy number is a positive integer, where the sum of the square of its digits eventually reaches 1 after repetition.
 -- In contrast, a sad number (not a happy number) is where the sum of the square of its digits never reaches 1
@@ -194,7 +215,17 @@ distinct =
 -- >>> isHappy 44
 -- True
 isHappy ::
-  Integer
+  Int
   -> Bool
-isHappy =
-  error "todo: Course.State#isHappy"
+isHappy = contains 1 . firstRepeat . (produce sumSquareOfDigits)
+
+sumSquareOfDigits :: Int -> Int
+sumSquareOfDigits i = sum (map (join (*)) (toDigits i))
+
+toDigits :: Int -> List Int
+toDigits x = reverse (toDigits0 x)
+  where 
+    toDigits0 i
+      | i > 0 = i `mod` 10 :. toDigits0 (i `div` 10)
+      | otherwise = Nil
+
